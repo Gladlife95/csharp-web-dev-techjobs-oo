c8bf4941263c0a886a671ea738543efdce6ce723
Jordan Glad <jordanglad8@gmail.com>
2022-01-10 14:57:21 -0600

    Finished assignment

--- TechJobsOO/Job.cs
+++ TechJobsOO/Job.cs
@@ -13,7 +13,60 @@ namespace TechJobsOO
         public CoreCompetency JobCoreCompetency { get; set; }
 
         // TODO: Add the two necessary constructors.
+        public Job()
+        {
+            Id = nextId;
+            nextId++;
+        }
+
+        public Job(string name,Employer employerName, Location employerLocation, PositionType jobType, CoreCompetency jobCoreCompetency) : this()
+        {
+            Name = name;
+            EmployerName = employerName;
+            EmployerLocation = employerLocation;
+            JobType = jobType;
+            JobCoreCompetency = jobCoreCompetency;
+        }
+
 
         // TODO: Generate Equals() and GetHashCode() methods.
+        public override bool Equals(object obj)
+        {
+            return obj is Job job &&
+                   Id == job.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+
+        public override string ToString()
+        {
+            if (Name == "" || Name == null)
+             {
+                 Name = "Data not available";
+             }
+            if (EmployerName.Value == "" || EmployerName.Value == null)
+             {
+                 EmployerName.Value = "Data not available";
+             }
+            if (EmployerLocation.Value == ""|| EmployerLocation.Value == null)
+             {
+                EmployerLocation.Value = "Data not available";
+             }
+            if (JobType.Value == "" || JobType.Value == null)
+             {
+                 JobType.Value = "Data not available";
+             }
+            if (JobCoreCompetency.Value == "" || JobCoreCompetency.Value == null)
+             {
+                 JobCoreCompetency.Value = "Data not available";
+             }
+            
+            string output =$"\nID: {Id}\nName: {EmployerName}\nLocation: {EmployerLocation}\nPosition Type: {JobType}\nCore Competency: {JobCoreCompetency}\n";
+            return output;
+        }
+      
     }
 }
